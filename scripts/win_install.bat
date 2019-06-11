70a7f8523bc0dca3bf9b13196dad1b6594fa3fbe
hallowf <alexandre.cortegaca@gmail.com>
2019-06-11 13:27:04 +0100

    Added more console outputs to windows deploy scripts

--- scripts/win_install.bat
+++ scripts/win_install.bat
@@ -5,13 +5,17 @@ venv\Scripts\activate.bat
 REM "Reinstalling requirements on virtualenv"
 python -m pip install -r requirements.txt
 REM "Building GUI"
-cd src
+cd src\
 pyinstaller --upx-dir=%UPX_PATH% gui.spec
 7z a STPDF-gui.7z dist > NUL:
-RMDIR /Q/S dist
-RMDIR /Q/S build
+REM "GUI BUILT"
+RMDIR /Q/S dist\
+RMDIR /Q/S build\
+REM "Building CLI"
 pyinstaller --upx-dir=%UPX_PATH% stpdf_cli.spec
 7z a STPDF-cli.7z dist > NUL:
-RMDIR /Q/S dist
-MKDIR dist
-xcopy *.7z dist
+REM "CLI BUILT"
+RMDIR /Q/S dist\
+MKDIR dist\
+REM "Copying files"
+xcopy *.7z dist\
