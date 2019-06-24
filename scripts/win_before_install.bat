ecf9ecdc99f32c380eee42f2cf372e850fa9a153
hallowf <alexandre.cortegaca@gmail.com>
2019-06-24 21:44:46 +0100

    Fixed pyqt version in build process

--- scripts/win_before_install.bat
+++ scripts/win_before_install.bat
@@ -1,8 +1,7 @@
 REM "Installing python"
 choco install python --version 3.4.4
 REM "Installing PyQt"
-choco install pyqt5 --version 5.1
-REM "Fetching UPX"
+choco install pyqt5
 mkdir UPX && cd UPX
 ECHO UPX_PATH is %cd%
 curl https://github.com/upx/upx/releases/download/v3.95/upx-3.95-win64.zip -J -L --output UPX.zip
