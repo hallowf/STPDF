5b27ad5c39232c12cf463bfc416c072ceb4f40bc
hallowf <alexandre.cortegaca@gmail.com>
2019-08-17 05:06:53 +0100

    Updated python version in travis, fixes #9

--- scripts/win_before_install.bat
+++ scripts/win_before_install.bat
@@ -1,5 +1,5 @@
 REM "Installing python"
-choco install python --version 3.5.4
+choco install python --version 3.7.3
 mkdir UPX && cd UPX
 ECHO UPX_PATH is %cd%
 curl https://github.com/upx/upx/releases/download/v3.95/upx-3.95-win64.zip -J -L --output UPX.zip
