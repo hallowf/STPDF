f2f0499b1912784a7144622095decf3f2b4f47d5
hallowf <alexandre.cortegaca@gmail.com>
2020-01-08 00:07:31 +0000

    Updated package installation on travis for linux

--- scripts/lin_before_install.sh
+++ scripts/lin_before_install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+echo "Installing packages"
+sudo apt -y install p7zip-full libpython3.7 libpython3.7-dev python3.7-dev
 mkdir UPX && cd UPX || return
 echo "UPX_PATH is $PWD"
 curl -sL -o upx.txz https://github.com/upx/upx/releases/download/v3.95/upx-3.95-amd64_linux.tar.xz
